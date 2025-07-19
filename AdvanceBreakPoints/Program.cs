3e20ba65693daa17a2853e6826861e83e7fa104b
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-19 23:20:28 +0800

    use davance vreak points to help you narrwo dwon issues and finally ifnd the locaiton fo the error.

---
+++ AdvanceBreakPoints/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+Console.ReadLine();
+
+static void RunsALot()
+{
+    long total = 0;
+    int test = 0;
+    for (int i = -1000; i <= 1000; i++)
+	{
+        total += i;
+		try
+		{
+			test = 5 / i;
+		}
+		catch 
+		{
+
+            Console.WriteLine("Ther was an exception");
+		}
+
+	}
+    Console.WriteLine($"The total is {total}");
+}
