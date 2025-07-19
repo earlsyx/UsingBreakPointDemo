fd01b9a3dac4a72e92211aa739c7453e5923ee84
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-19 17:40:56 +0800

    thoew new exepcitonss whw

---
+++ HWHandlignExceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int[] ages = new int[] { 1, 3, 5, 7 };
+for (int i = 0; i <= ages.Length; i++)
+{
+    try
+    {
+        Console.WriteLine(ages[i]);
+    }
+    catch(Exception ex)
+    {
+        throw new Exception("There was an error at ", ex);
+    }
+   
+}
